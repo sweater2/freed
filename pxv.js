eea7e825a329ce9e18929654b11abbce2946c659
sweater2 <99777656+sweater2@users.noreply.github.com>
2024-02-18 00:41:46 +0800

    Add files via upload

--- pxv.js
+++ pxv.js
@@ -7,24 +7,37 @@
 // @match        https://www.pixiv.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/sweater2/freed/master/pxv.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
 
-    setInterval(function() {
+    setInterval(function () {
         // Array.from(document.querySelectorAll('div.list-item.column-2')).filter(n => !n.innerHTML.includes('ugoira')).map(n => n.remove())
-        const durl = document.location.href
-        if (durl.match(/pixiv\.net\/artworks\/\d+$/g) !== null) {
-            window.location.replace(durl+'#manga')
-        }
+
+        // 换成下面的点击按钮了
+        // const durl = document.location.href
+        // if (durl.match(/pixiv\.net\/artworks\/\d+$/g) !== null) {
+        //     window.location.replace(durl + '#manga')
+        // }
+        const btn = document.querySelectorAll('a div.button-link')
+        if (btn.length > 0) { btn[0].click() }
+
+        Array.from(document.querySelectorAll('div.list-item.column-2')).map(n => {
+            if (n.getAttribute('ponyproc') !== null) return
+            n.style['width'] = '51%'
+            n.style['float'] = 'right'
+            n.setAttribute('ponyproc', 1)
+        })
 
         Array.from(document.querySelectorAll('a')).filter(n => n.href.includes('pixiv.net/artworks/')).map(n => {
             const url = n.href
+            n.target = '_blank'
             if (url.match(/artworks\/\d+$/g) === null) return
-            n.href = url+'#manga'
+            n.href = url + '#manga'
         })
     }, 1000)
 })();
